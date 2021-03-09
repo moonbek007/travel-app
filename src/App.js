5a448ec4793d8f582e6eb46cb61e6da1307d2dcb
Ruslan Nagiev <ultraduck100@gmail.com>
2021-03-09 22:38:18 +0300

    fix: fix route swhitch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import CountryPage from "./components/Pages/CountryPage";
 import Main from "./components/Main/Main";
@@ -10,8 +10,10 @@ function App() {
   return (
     <Router>
       <div className='app'>
-        <Route path='/test' component={CountryPage} />
-        <Route path='/main' component={Main} />
+        <Switch>
+          <Route path='/country' component={CountryPage} />
+          <Route path='/' component={Main} />
+        </Switch>
         <Footer />
       </div>
     </Router>
