e69dab916cfaf5203fbf63be1ee569ab5927abf1
Ruslan Nagiev <ultraduck100@gmail.com>
2021-03-09 04:46:11 +0300

    feat: add simple routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import './App.css';
-import Main from './components/Main/Main';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Main from "./components/Main/Main";
+import CountryPage from "./components/Pages/CountryPage";
 
 function App() {
   return (
-    <div className='app'>
-      <Main />
-    </div>
+    <Router>
+      <header>
+        this is not a real heder, just a demonstration
+      </header>
+      <div className='app'>
+        <Route path='/test' component={CountryPage} />
+        <Route path='/main' component={Main} />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Main/Card/Card.js
+++ src/components/Main/Card/Card.js
@@ -1,21 +1,24 @@
 import React from "react";
 import s from "./card.module.css";
+import { Link } from "react-router-dom";
 
 const Card = ({ imgUrl, country, capital }) => {
   return (
-    <div 
-      className={s.wrapper}
-      style={{ backgroundImage: `url(${imgUrl})` }}
-    >
-      <div className={s.inner}>
-        <div className={s.country}>
-          <span className={s.location}>{country}</span>
+    <Link to='/test'> 
+      <div 
+        className={s.wrapper}
+        style={{ backgroundImage: `url(${imgUrl})` }}
+      >
+        <div className={s.inner}>
+          <div className={s.country}>
+            <span className={s.location}>{country}</span>
+          </div>
+          <div className={s.capital}>
+            <span className={s.location}>{capital}</span>
+          </div> 
         </div>
-        <div className={s.capital}>
-          <span className={s.location}>{capital}</span>
-        </div> 
       </div>
-    </div>
+    </Link>
   );
 };
 
