1ecdd80be6c1d6c2f4d6fd0df97a9d0e201abfc2
moonbek <oybek4ik.mamatov.00@mail.ru>
2021-03-10 17:17:04 +0500

    refactor: changing logo links to local links

--- src/components/Header/CardHeader.js
+++ src/components/Header/CardHeader.js
@@ -1,13 +1,13 @@
 import React from "react";
 import headerStyles from "./header.module.css";
+import leftArrowLogo from "../../assets/left_arrow.svg";
+import rightArrowLogo from "../../assets/right_arrow.svg";
+import travelAppLogo from "../../assets/travel_app_logo.png";
 function CardHeader() {
   return (
     <header>
       <div className={headerStyles.rowOne}>
-        <img
-          src="https://i.pinimg.com/originals/36/3d/e7/363de73bdfcbb047c5d8f1786389c818.png"
-          alt="travel-logo"
-        />
+        <img src={travelAppLogo} alt="travel-logo" />
         <select name="languages" id="languages" value="EN">
           <option value="EN">EN</option>
           <option value="RU">RU</option>
@@ -16,16 +16,10 @@ function CardHeader() {
       </div>
       <div className={headerStyles.rowTwo}>
         <button>
-          <img
-            src="https://www.flaticon.com/svg/vstatic/svg/271/271220.svg?token=exp=1615356068~hmac=0d65a23971e0da2f8e57630378aa113d"
-            alt="left-arrow-logo"
-          />
+          <img src={leftArrowLogo} alt="left-arrow-logo" />
         </button>
         <button>
-          <img
-            src="https://www.flaticon.com/svg/vstatic/svg/271/271228.svg?token=exp=1615356440~hmac=737f4d25bb8734bc5bd06ebf965efea2"
-            alt="right-arrow-logo"
-          />
+          <img src={rightArrowLogo} alt="right-arrow-logo" />
         </button>
       </div>
       <div className={headerStyles.rowThree}></div>
