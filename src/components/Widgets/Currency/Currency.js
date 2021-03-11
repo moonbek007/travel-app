a302d672d930e8e86777f8903cd807acd7744779
Alek_Ring <okolcov60@gmail.com>
2021-03-11 09:58:23 +0300

    feat: add currency

--- src/components/Widgets/Currency/Currency.js
+++ src/components/Widgets/Currency/Currency.js
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import s from "./currency.module.css";
+
+// Import api service for fetching currency
+import { apiService } from '../../../services/api.service'
 
 const Currency = () => {
 
-  const toUsd = '1.1798';
-  const toEur = '0.3650';
-  const toRub = '27.1798';
+  const [ USDrate, SetUSDRate ] = useState(1);
+  const [ EURrate, SetEURRate ] = useState(2);
+  const [ RUBrate, SetRUBRate ] = useState(3);
+
+  async function getRates() {
+    const response = await apiService.fetchPosts()
+
+    SetUSDRate(response.rates.USD)
+    SetEURRate(response.rates.EUR)
+    SetRUBRate(response.rates.RUB)
+    
+    // console.log(response.rates)
+  }
+
+  useEffect(() => {
+    getRates()
+  }, [])
+
 
   return (
-    <div className={s.wrapper}>
-      <div className={s.item}>USD: {toUsd}</div>
-      <div className={s.item}>EUR: {toEur}</div>
-      <div className={s.item}>RUB: {toRub}</div>
+    <div className={s.currency}>
+        <h4>
+          currency
+        </h4>
+        <div className={s['rates-block']}>
+          <div className={s['usd']}>
+            USD: {USDrate}
+          </div>
+          <div className={s['eur']}>
+            EUR: {EURrate}
+          </div>
+          <div className={s['rub']}>
+            RUB: {RUBrate}
+          </div>
+        </div>
     </div>
   );
 };
---
+++ src/services/api.service.js
@@ -0,0 +1,23 @@
+class ApiService {
+    constructor(url) {
+        this.url = url;
+    }
+
+    async fetchPosts() {
+        try {
+            const request = new Request(this.url, {
+                method: 'get'
+            });
+            return sendRequest(request);
+        } catch (error) {
+            console.log(console.error());
+        };
+    }
+}
+
+async function sendRequest(request) {
+    const response = await fetch(request);
+    return await response.json();
+}
+
+export const apiService = new ApiService('https://openexchangerates.org/api/latest.json?app_id=7ac3c53d982242239ff824ac7653218c')
