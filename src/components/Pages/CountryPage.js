c1b8435aa09534d5e06012feb4773a271a2f5e0b
moonbek <oybek4ik.mamatov.00@mail.ru>
2021-03-11 17:04:57 +0500

    feat: add photogallery on CountryPage
    added react-image-gallery dependency , imported it in CountryPage.js and added temporary photos to it

--- src/components/Pages/CountryPage.js
+++ src/components/Pages/CountryPage.js
@@ -5,22 +5,41 @@ import ShortInfo from "../ShortInfo/ShortInfo";
 
 import Widgets from "../Widgets/Widgets";
 import MapWidget from "./map/Map";
-
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+const images = [
+  {
+    original:
+      "https://images.pexels.com/photos/1797161/pexels-photo-1797161.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    thumbnail:
+      "https://images.pexels.com/photos/1797161/pexels-photo-1797161.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    original:
+      "https://images.pexels.com/photos/374870/pexels-photo-374870.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    thumbnail:
+      "https://images.pexels.com/photos/374870/pexels-photo-374870.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=420&w=320",
+  },
+];
 const CountryPage = () => {
   return (
     <div>
       <div className={s.main}>
         <Widgets />
         <ShortInfo />
-        <div className={s.photoBox}>Photogalery</div>
+        <div className={s.photoBox}>Photogallery</div>
         <div className={s.mapBox}>
           <div className={s.map}>
             <MapWidget />
           </div>
-          <div className={s.text}>
-            some text
-          </div>
+          <div className={s.text}>some text</div>
         </div>
+        <ImageGallery
+          items={images}
+          showBullets={true}
+          showIndex={true}
+          thumbnailPosition={"bottom"}
+        />
       </div>
     </div>
   );
