69e9ef486beca6cf9eeff2ef4e86370308c90eef
leeshko <pumpkin@tut.by>
2021-03-13 22:26:10 +0300

    feat: make countries border and capital pointer

--- src/components/Pages/map/Map.js
+++ src/components/Pages/map/Map.js
@@ -1,17 +1,50 @@
-import { YMaps, Map } from 'react-yandex-maps';
-import s from './map.module.css'
+import React from 'react';
+import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import st from './map.module.css';
 
+const mapState = {
+  center: [53.902496, 27.561481],
+  zoom: 5, 
+  region: "BY"
+};
 
-const MapWidget = () => (
-    <div className={s.layer}>
-  <YMaps>
-      <Map 
-      defaultState={{ center: [55.75, 37.57], zoom: 9 }} 
-      height="100%"
-      width="100%"
-      />
-  </YMaps>
+function MapWidget() {
+
+  const mapRef = React.createRef(null);
+  const lang = "ru_RU";   // "en_US" - english   "tr_TR" - turkish
+  
+  const getRegions = ymaps => {
+    if (mapRef && mapRef.current) {
+  
+      ymaps.borders
+        .load("001", {
+          quality: 2
+        })
+        .then(function (result) {
+          let country = result.features.find(item => item.properties.iso3166 === mapState.region); 
+          console.log(country)
+          let geoObject = new ymaps.GeoObject(country);
+          mapRef.current.geoObjects.add(geoObject);
+        });
+    }
+  };
+
+  return (
+    <div className={st.layer}>
+      <YMaps key={lang} query={{ lang }}>
+        <Map
+          height="100%"
+          width="100%"
+          instanceRef={mapRef}
+          defaultState={mapState}
+          onLoad={ymaps => getRegions(ymaps)}
+          modules={["borders", "GeoObject"]}
+        >
+          <Placemark geometry={mapState.center} />
+        </ Map>
+      </YMaps>
     </div>
-);
+  );
+}
 
 export default MapWidget;
